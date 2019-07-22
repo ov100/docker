32a67cf0ba56f9d28600b85ae2d77fd49fd6c97c
bo.gao <bo.gao@mtime.com>
2019-07-22 16:51:43 +0800

    更改项目名称

--- docker-web-demo/src/test/java/cn/robustsoft/docker/demo/DockerWebDemoApplicationTest.java
+++ docker-web-demo/src/test/java/cn/robustsoft/docker/demo/DockerWebDemoApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DockerSimpleWebDemoApplicationTests {
+public class DockerWebDemoApplicationTest {
 
     @Test
     public void contextLoads() {
